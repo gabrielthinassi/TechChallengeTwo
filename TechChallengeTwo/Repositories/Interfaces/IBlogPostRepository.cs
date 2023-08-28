5c5279d1fe2daf6f4633f4a4119864a15712a90a
gabrielthinassi <gabriel.m.thinassi@gmail.com>
2023-08-28 15:01:35 -0300

    Removing Repository

--- TechChallengeTwo/Repositories/Interfaces/IBlogPostRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Linq.Expressions;
-using TechChallengeTwo.Models.Entities.Blog;
-
-namespace profilefinder_back.Repositories.Interfaces
-{
-    public interface IBlogPostRepository : IBaseRepository<BlogPost>
-    { }
-}
