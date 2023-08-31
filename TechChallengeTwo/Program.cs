0c3577bb15351e4ebea24229d5b2c3bae6cf3dc7
gabrielthinassi <gabriel.m.thinassi@gmail.com>
2023-08-31 10:18:10 -0300

    feat: Dockerizing the project - Add mssql on Compose

--- TechChallengeTwo/Program.cs
+++ TechChallengeTwo/Program.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TechChallengeTwo.Data;
+using TechChallengeTwo.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//var server = builder.Configuration["DbServer"] ?? "localhost";
+//var port = builder.Configuration["DbPort"] ?? "1450";
+var user = builder.Configuration["DbUser"] ?? "SA";
+var password = builder.Configuration["DbPassword"] ?? "Gmtpostech#2023";
+var database = builder.Configuration["Database"] ?? "BlogDB";
+var datasource = builder.Configuration["DataSource"] ?? "techchallengetwodb";
+var connectionString = $"Data Source={datasource};Initial Catalog={database};User ID={user};Password={password}";
+//var connectionString = $"Server={server}, {port};Initial Catalog={database};User ID={user};Password={password}";
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -27,6 +37,8 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+DatabaseManagementService.MigrationInitialisation(app);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
